4e64886d1f493cde828b3acd01d7986e92a13ea3
Katy Fernelius <kkin001@gmail.com>
2022-07-26 12:59:36 -0500

    filter functionality for flow builder is working

--- Scripts/flow-builder-scripts/helper-scripts.js
+++ Scripts/flow-builder-scripts/helper-scripts.js
@@ -2,6 +2,21 @@ const YOGA_API = 'https://lightning-yoga-api.herokuapp.com/yoga_poses';
 
 //Gets all poses from the yoga api
 export function initialGet() {
+  cardCreate(
+    'Repeat',
+    'Repeat all previous moves',
+    '../images/icons8-repeat-64.png'
+  );
+  cardCreate(
+    'Right Side',
+    'Do the following movements on the right side',
+    '../images/icons8-right-arrow-64.png'
+  );
+  cardCreate(
+    'Left Side',
+    'Do the following movements on the left side',
+    '../images/icons8-left-arrow-64.png'
+  );
   fetch(YOGA_API)
     .then((res) => res.json())
     .then((data) => {
@@ -15,7 +30,7 @@ export function initialGet() {
 }
 
 //used to create the cards and append them to their container
-function cardCreate(title, text, img) {
+export function cardCreate(title, text, img) {
   var newDiv = document.createElement('div');
   newDiv.setAttribute('class', 'card');
   newDiv.innerHTML = `
@@ -65,3 +80,5 @@ export function saveFlow() {
     console.log(allPoses);
   }
 }
+
+export function removeNodes(parentElm) {}
