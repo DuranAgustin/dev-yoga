913898db2bef08ee464f7cba0e077d114ea1a5bc
Agustin <agustinduranse@gmail.com>
2022-07-27 15:42:10 -0700

    dashboard-ad

--- CRUD/delete-flows.js
+++ CRUD/delete-flows.js
@@ -1,11 +1,11 @@
 function handleRemove(event) {
   console.log(event.getAttribute("db-id"));
-
   let ID = event.getAttribute("db-id");
-  const serverURL = `https://dev-yoga-api.herokuapp.com/`;
+
+  const serverURL = `https://dev-yoga-api.herokuapp.com/` + ID;
   //console.log(ID);
   //62e0afe6161788125c6849bf
-  fetch("https://dev-yoga-api.herokuapp.com/", {
+  fetch("https://dev-yoga-api.herokuapp.com/" + ID, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
