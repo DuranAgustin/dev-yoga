3609bef592fd9a38d893cd5cfdce37a63979777d
Agustin <agustinduranse@gmail.com>
2022-07-27 14:34:09 -0700

    getting card-id to delete

---
+++ CRUD/delete-flows.js
@@ -0,0 +1,17 @@
+function handleRemove(event) {
+  console.log(event.getAttribute("db-id"));
+
+  let ID = event.getAttribute("db-id");
+  const serverURL = `https://dev-yoga-api.herokuapp.com/`;
+  //console.log(ID);
+  //62e0afe6161788125c6849bf
+  fetch("https://dev-yoga-api.herokuapp.com/", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(ID),
+  })
+    .then((res) => res.text()) // or res.json()
+    .then((res) => console.log(res));
+}
--- Scripts/flow-builder-scripts/helper-scripts.js
+++ Scripts/flow-builder-scripts/helper-scripts.js
@@ -1,23 +1,23 @@
-import { submitFunc } from '../../CRUD/post-flows.js';
+import { submitFunc } from "../../CRUD/post-flows.js";
 
-const YOGA_API = 'https://lightning-yoga-api.herokuapp.com/yoga_poses';
+const YOGA_API = "https://lightning-yoga-api.herokuapp.com/yoga_poses";
 
 //Gets all poses from the yoga api
 export function initialGet() {
   cardCreate(
-    'Repeat',
-    'Repeat all previous moves',
-    '../images/icons8-repeat-64.png'
+    "Repeat",
+    "Repeat all previous moves",
+    "../images/icons8-repeat-64.png"
   );
   cardCreate(
-    'Right Side',
-    'Do the following movements on the right side',
-    '../images/icons8-right-arrow-64.png'
+    "Right Side",
+    "Do the following movements on the right side",
+    "../images/icons8-right-arrow-64.png"
   );
   cardCreate(
-    'Left Side',
-    'Do the following movements on the left side',
-    '../images/icons8-left-arrow-64.png'
+    "Left Side",
+    "Do the following movements on the left side",
+    "../images/icons8-left-arrow-64.png"
   );
   fetch(YOGA_API)
     .then((res) => res.json())
@@ -33,8 +33,8 @@ export function initialGet() {
 
 //used to create the cards and append them to their container
 export function cardCreate(title, text, img) {
-  var newDiv = document.createElement('div');
-  newDiv.setAttribute('class', 'card');
+  var newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "card");
   newDiv.innerHTML = `
   <div class = "card-body">
   <img class='card-img-top' src='${img}' alt='card image top'>
@@ -43,52 +43,52 @@ export function cardCreate(title, text, img) {
   </div>
   `;
 
-  newDiv.addEventListener('click', () => {
+  newDiv.addEventListener("click", () => {
     addToList(newDiv);
   });
-  document.getElementById('pose-grid').appendChild(newDiv);
+  document.getElementById("pose-grid").appendChild(newDiv);
 }
 
 export function cardCreateNoImg(title, id) {
-  var newDiv = document.createElement('div');
-  newDiv.setAttribute('class', 'card');
-  newDiv.setAttribute('id', 'flow-card');
-  newDiv.setAttribute('style', 'width: 18rem');
-  newDiv.setAttribute('db-id', `${id}`);
+  var newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "card");
+  newDiv.setAttribute("id", "flow-card");
+  newDiv.setAttribute("style", "width: 18rem");
+  newDiv.setAttribute("db-id", `${id}`);
   newDiv.innerHTML = `
   <div class="card-body">
   <h5 class="card-title">${title}</h5>
-  <button class="btn-primary">Start Flow</button>
-  <button class="btn-primary">Delete Flow</button>
+  <button class="btn btn-primary">Start Flow</button>
+  <button class="btn btn-secondary" ids ="" onclick="handleRemove(this.parentNode.parentNode)">Delete Flow</button>
   </div>
   `;
-  document.getElementById('saved-flows').appendChild(newDiv);
+  document.getElementById("saved-flows").appendChild(newDiv);
 }
 //function to add the node to the pose list for the flow
 export function addToList(node) {
   var copyDiv = node.cloneNode();
   copyDiv.innerHTML = node.innerHTML;
-  copyDiv.setAttribute('class', 'card newCard');
-  copyDiv.addEventListener('click', () => {
+  copyDiv.setAttribute("class", "card newCard");
+  copyDiv.addEventListener("click", () => {
     copyDiv.remove();
   });
-  document.getElementById('new-flow-container').appendChild(copyDiv);
+  document.getElementById("new-flow-container").appendChild(copyDiv);
 }
 
 //used to save the flow once the user has built their desired workout
 export function saveFlow() {
-  let collectionTitle = prompt('Please provide a title for your new flow');
+  let collectionTitle = prompt("Please provide a title for your new flow");
   if (collectionTitle) {
     let allPoses = [];
     const poses = Array.from(
-      document.getElementById('new-flow-container').childNodes
+      document.getElementById("new-flow-container").childNodes
     );
 
     poses.forEach((element) => {
       const poseObj = {
-        poseName: element.querySelector('.card-title').innerHTML,
-        poseDescription: element.querySelector('.card-text').innerHTML,
-        poseImage: element.querySelector('.card-img-top').src,
+        poseName: element.querySelector(".card-title").innerHTML,
+        poseDescription: element.querySelector(".card-text").innerHTML,
+        poseImage: element.querySelector(".card-img-top").src,
       };
       allPoses.push(poseObj);
       element.remove();
