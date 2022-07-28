4976c3fe506eb0af9680fc2b0fce3bf576bfb8c6
Agustin <agustinduranse@gmail.com>
2022-07-27 19:20:47 -0700

    nope

--- CRUD/delete-flows.js
+++ CRUD/delete-flows.js
@@ -2,16 +2,16 @@ function handleRemove(event) {
   console.log(event.getAttribute("db-id"));
   let ID = event.getAttribute("db-id");
 
-  const serverURL = `https://dev-yoga-api.herokuapp.com/` + ID;
+  // const serverURL = `https://dev-yoga-api.herokuapp.com/` + ID;
   //console.log(ID);
   //62e0afe6161788125c6849bf
-  fetch("https://dev-yoga-api.herokuapp.com/" + ID, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(ID),
-  })
-    .then((res) => res.text()) // or res.json()
-    .then((res) => console.log(res));
+  // fetch("https://dev-yoga-api.herokuapp.com/" + ID, {
+  //   method: "DELETE",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(ID),
+  // })
+  //   .then((res) => res.text()) // or res.json()
+  //   .then((res) => console.log(res));\function deleteListingByName(client, nameOfListing) {
 }
